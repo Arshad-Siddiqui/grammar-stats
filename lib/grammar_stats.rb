ee57f394d0ac1fdd13fd917cfe0f8c5b41f9462e
Arshad Siddiqui <ferozarshad123@gmail.com>
2022-09-15 17:14:52 +0100

    Complete GrammarStats tests

---
+++ lib/grammar_stats.rb
@@ -0,0 +1,20 @@
+class GrammarStats
+  def initialize
+    @checks = []
+  end
+
+  def check(text) # text is a string
+    @checks.push text.match? /^[A-Z][\w\s]*[?.!]$/
+    @checks[-1]
+  end
+
+  def percentage_good
+    if @checks.length == 0 then return 0 end
+    passes = 0
+    fails = 0
+    @checks.each do |result|
+      result ? passes += 1 : fails += 1     
+    end
+    (passes.to_f / (passes.to_f + fails.to_f)) * 100
+  end
+end
--- spec/grammar_stats_spec.rb
+++ spec/grammar_stats_spec.rb
@@ -1,1 +1,38 @@
 require 'grammar_stats'
+
+RSpec.describe GrammarStats do
+  describe '#percentage_good' do
+    it 'Returns 0 when handed nothing' do
+      grammar_tracker = GrammarStats.new
+      expect(grammar_tracker.percentage_good).to eq 0
+    end
+
+    it 'Returns 100 when handed 1 good grammar' do
+      grammar_tracker = GrammarStats.new
+      grammar_tracker.check('Hello world.')
+      expect(grammar_tracker.percentage_good).to eq 100
+    end
+
+    it "Returns 0 when handed 1 bad grammar" do
+      grammar_tracker = GrammarStats.new
+      grammar_tracker.check('hello world')
+      expect(grammar_tracker.percentage_good).to eq 0
+    end
+
+    it "returns 50 when handed a good and a bad grammar" do
+      grammar_tracker = GrammarStats.new
+      grammar_tracker.check('hello world')
+      grammar_tracker.check('Hello world.')
+      expect(grammar_tracker.percentage_good).to eq 50
+    end
+
+    it 'returns 75 when handed 3 good 1 bad' do
+      grammar_tracker = GrammarStats.new
+      grammar_tracker.check('hello world')
+      grammar_tracker.check('Hello world.')
+      grammar_tracker.check('Hello world.')
+      grammar_tracker.check('Hello world.')
+      expect(grammar_tracker.percentage_good).to eq 75
+    end
+  end
+end
